62aeabcacf5b52667e0fc7e0eeb6d35292f8afe5
ka-jun <teraguchi51@gmail.com>
2021-03-27 15:41:05 +0900

    ログインしている時に自分が投稿したものを編集、削除表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    # binding.pry
     @comment = Comment.create(comment_params)
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
